6b2fe7be418514cf55e1eb55c23d0ce52611fb24
Max Lefebvre <max.lefebvre@garvin-allen.com>
2022-04-08 15:57:10 -0300

    updated library to allow overriding the default HTML conversion behavior - pt.2

--- Maxle5.ProseMirror/ProseMirrorConvert.cs
+++ Maxle5.ProseMirror/ProseMirrorConvert.cs
@@ -1,6 +1,9 @@
-﻿using Maxle5.ProseMirror.Models;
+﻿using Maxle5.ProseMirror.Builders;
+using Maxle5.ProseMirror.Models;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Maxle5.ProseMirror
 {
@@ -20,9 +23,11 @@ namespace Maxle5.ProseMirror
             });
         }
 
-        public static Node DeserializeObjectFromHtml(string html)
+        public static Node DeserializeObjectFromHtml(
+            string html, 
+            IEnumerable<INodeBuilder> customNodeBuilders = null)
         {
-            return new HtmlConverter().Convert(html);
+            return new HtmlConverter(customNodeBuilders).Convert(html);
         }
 
         public static Node DeserializeObjectFromJson(string json)
